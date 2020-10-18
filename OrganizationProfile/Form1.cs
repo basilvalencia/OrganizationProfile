b7a03761e069bb1c5fa00b3cd03179b92fc53717
Basil Valencia <basildonvalencia@gmail.com>
2020-10-18 22:54:01 +0800

    Initial Commit

---
+++ OrganizationProfile/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace OrganizationProfile
+{
+    public partial class frmRegistration : Form
+    {
+        private string _FullName;
+        private int _Age;
+        private long _ContactNo;
+        private long _StudentNo;
+
+        public long StudentNumber(string studNum)
+        {
+
+            _StudentNo = long.Parse(studNum);
+
+            return _StudentNo;
+        }
+
+        public long ContactNo(string Contact)
+        {
+            if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+            {
+                _ContactNo = long.Parse(Contact);
+            }
+
+            return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+            {
+                _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+            }
+
+            return _FullName;
+        }
+
+        public int Age(string age)
+        {
+            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+            {
+                _Age = Int32.Parse(age);
+            }
+
+            return _Age;
+        }
+
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmRegistration_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            StudentInformationClass.SetFullName = FullName(txtLastName.Text, txtFirstName.Text, txtMiddleInitial.Text);
+            StudentInformationClass.SetStudentNo = StudentNumber(txtStudentNo.Text);
+            StudentInformationClass.SetProgram = cbPrograms.Text;
+            StudentInformationClass.SetGender = cbGender.Text;
+            StudentInformationClass.SetContactNo = ContactNo(txtContactNo.Text);
+            StudentInformationClass.SetAge = Age(txtAge.Text);
+            StudentInformationClass.SetBirthday = datePickerBirthday.Value.ToString("yyyy-MM-dd");
+
+            frmConfirmation frm = new frmConfirmation();
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                txtStudentNo.ResetText();
+                cbPrograms.ResetText();
+                txtLastName.ResetText();
+                txtLastName.ResetText();
+                txtMiddleInitial.ResetText();
+                txtAge.ResetText();
+                txtContactNo.ResetText();
+                cbGender.ResetText();
+            }
+            else
+            {
+                frm.Dispose();
+            }
+        }
+
+        private void frmRegistration_Load_1(object sender, EventArgs e)
+        {
+            string[] ListOfProgram = new string[] {
+                    "BS Information Technology",
+                    "BS Computer Science",
+                    "BS Information System",
+                    "BS in Accountancy",
+                    "BS in Hospitality Management",
+                    "BS in Tourism Management"
+            };
+
+            for (int i = 0; i < ListOfProgram.Length; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+            
+                string[] ListOfGender = new string[]
+               {
+                "Male",
+                "Female"
+               };
+
+                for (int g = 0; g < ListOfGender.Length; g++)
+                {
+                    cbGender.Items.Add(ListOfGender[g].ToString());
+                }
+            }
+        }
+    }
+
+
---
+++ OrganizationProfile/StudentInformationClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrganizationProfile
+{
+    class StudentInformationClass
+    {
+        public static long SetStudentNo = 0;
+        public static long SetContactNo = 0;
+        public static int SetAge = 0;
+        public static string SetProgram = " ";
+        public static string SetGender = " ";
+        public static string SetBirthday = " ";
+        public static string SetFullName = " ";
+    }
+}
